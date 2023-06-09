7133a5087e479eabfe6ac76167ca86872534b143
Jonah Bloch-Johnson <me@jonah.org>
2023-06-09 10:21:19 -0500

    added icon

--- js/place.js
+++ js/place.js
@@ -636,9 +636,9 @@ function showTimeSeries() {
   var y = document.getElementById("timeSeriesButton");
   if (x.style.display === "none") {
     x.style.display = "block";
-    y.innerHTML = "Hide time series"
+    y.innerHTML = "Hide time series&nbsp;&nbsp;<i class=\"fas fa-chart-line\"></i>"
   } else {
     x.style.display = "none";
-    y.innerHTML = "Show time series"
+    y.innerHTML = "Show time series&nbsp;&nbsp;<i class=\"fas fa-chart-line\"></i>"
   }
 } 
